f0ceb2ad6f1678068fb19c40e8c5b995727d43d0
pedrecho <pedrecho.flash@gmail.com>
2022-09-22 18:23:00 +0300

    v1.0

--- server.py
+++ server.py
@@ -1,20 +1,42 @@
 import socket
+from threading import Thread
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002
+separator_token = "<SEP>"
 
-msg = ''
+client_sockets = set()
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+
+def listen_for_client(cs):
+    while True:
+        try:
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            msg = msg.replace(separator_token, ": ")
+        finally:
+            print(msg)
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
+
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    client_socket, client_address = s.accept()
+    print(f"[+] {client_address} connected.")
+    client_sockets.add(client_socket)
+    t = Thread(target=listen_for_client, args=(client_socket,))
+    t.daemon = True
+    t.start()
 
-print(msg)
 
-conn.close()
+for cs in client_sockets:
+    cs.close()
+s.close()
